e35aeac1af0abe636357418f77b619ccc5a406df
Josjs <josjs@online.no>
2020-02-10 12:09:23 +0100

    help me

--- radar.py
+++ radar.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 
 import numpy as np
+import uRAD
 
 print("Hei, sveis. {}".format(np.pi))
+
+uRAD.loadConfiguration(1, 25, 0, 200, 5, 15, 0, 4)
+uRAD.turnON()
+
+velocity = [0]
+snr = [0]
+
+while True:
+    uRAD.detection(0, velocity, snr, 0, 0, 0)
+    print("velocity: {}, snr: {}".format(velocity[0], snr[0]))
 
