e802eff251867fd6f0dd8e9b82a89db0fd334609
Josjs <josjs@online.no>
2020-02-17 10:39:22 +0100

    More things

--- radar.py
+++ radar.py
@@ -2,6 +2,13 @@
 
 import numpy as np
 import uRAD
+import argparse
+import datetime
+
+parser = argparse.ArgumentParser()
+parser.add_argument("points", type=int, default = 0,
+                    help = "number of datapoints to take")
+args = parser.parse_args()
 
 # uRAD.detection takes an array for each measurement it provides.
 # The format is:
@@ -11,7 +18,7 @@ import uRAD
 
 # Radar input parameters
 mode = 1   # 1 = doppler mode
-f0   = 15  # starting at 24.015 GHz
+f0   = 100  # starting at 24.1 GHz
 BW   = 240 # using all the BW available = 240 MHz
 Ns   = 200 # 200 samples
 Ntar = 1   # only one target of interest
@@ -29,11 +36,12 @@ qarr            = [0] * Ns
 
 # Output directory
 outdir = "./output/"
-out_i = "I_CW.csv"
-out_q = "Q_CW.csv"
+time = datetime.datetime.now().isoformat()
+out_i = "I_CW_{}.csv".format(time)
+out_q = "Q_CW_{}.csv".format(time)
 
 # Number of datapoints to log, 0 will run forever
-ndata = 100
+ndata = args.points
 
 # Append one sample to csv file, 'safer' than overwriting
 def datappend(sample, path):
@@ -43,6 +51,10 @@ def datappend(sample, path):
     csv_string += str(sample[-1:][0]) + "\n"
     with open(path, 'a') as file:
         file.write(csv_string)
+
+def datawrite(path):
+    with open(path, 'w') as file:
+        #do nothing
         
 ##############################################################################
 
@@ -50,12 +62,18 @@ uRAD.loadConfiguration(mode, f0, BW, Ns, Ntar, Rmax, MTI, Mth)
 
 uRAD.turnON()
 
+datawrite(outdir + out_i)
+datawrite(outdir + out_q)
+
 i = 0
 while (i < ndata or not ndata):
     uRAD.detection(0, velocity, snr, iarr, qarr, movement)
     if movement[0]==True:
         print("velocity: {: 6.2f}, snr: {: 6.2f}".format(velocity[0], snr[0]))
-        datappend(iarr, outdir + out_i)
-        datappend(qarr, outdir + out_q)
-        i += 1
+    else:
+        print(i)
+    datappend(iarr, outdir + out_i)
+    datappend(qarr, outdir + out_q)
+    i += 1
 
+uRAD.turnOFF()
